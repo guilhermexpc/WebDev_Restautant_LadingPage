29d7620e36debdbd02368ae1259eb3ed5cec6d42
Guilherme <guilhermee1219@gmail.com>
2025-02-25 01:59:24 -0300

    Create Products Section with dynamic product card item

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,56 @@ $(document).ready(() => {
   $(mobileBtnMenu).on("click", () => {
     $(mobileNavMenu).toggleClass('mobile-menu-active');
   })
-})
+});
+
+function loadProducts() {
+  $.getJSON("products.json", (item) => {
+    if (!item || item.length === 0) {
+      console.error("Nenhum produto encontrado!");
+      return;
+    };
+
+    // Cria os cards de produtos dinamicamente
+    item.forEach(() => {
+      $('#product-card').append('<div class="product-card-item"></div>'); // Adiciona os cards vazios
+    });
+
+    $('.product-card-item').each(function (index) {
+      $(this).load('card.html', function () {
+        // Preenche os dados do produto carregado
+        $(this).find('img').attr('src', item[index].img);
+        $(this).find('.product-card-title').text(item[index].title);
+        $(this).find('.product-card-description').text(item[index].description);
+        $(this).find('.product-card-value').text(item[index].price);
+        $(this).find('.product-card-rating-reviews').text(item[index].reviews);
+        // Gera as estrelas
+        let ratingHtml = "";
+        for (let i = 0; i < 5; i++) {
+          if (i < item[index].rating) {
+            ratingHtml += '<i class="fa-solid fa-star"></i>';
+          } else {
+            ratingHtml += '<i class="fa-regular fa-star"></i>';
+          }
+        }
+        $(this).find('.product-card-rating-stars li').html(ratingHtml);
+
+      });
+    });
+  }).fail(function () {
+    console.error("Erro ao carregar os produtos!");
+  });
+}
+
+$(document).ready(function () {
+  loadProducts();
+});
+
+// Função para atualizar o tamanho da tela
+function updateScreenSize() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  document.getElementById('screenSize').textContent = 'Screen Size ' + width + 'px  x ' + height + 'px';
+}
+updateScreenSize();
+// Atualiza o tamanho da tela sempre que a janela for redimensionada
+window.onresize = updateScreenSize;
